305b2a3357543045caea07884f5475141f372ed6
xujia <j538350@163.com>
2025-03-12 23:47:11 +0800

    test

--- backend_django/users/tests.py
+++ backend_django/users/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from users.models import User,Role
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    def test_db_table_name(self):
+        """测试User模型的数据库表名是否正确设置"""
+        # 获取User模型的Meta类
+        user_meta = User._meta
+        print(user_meta.db_table)
