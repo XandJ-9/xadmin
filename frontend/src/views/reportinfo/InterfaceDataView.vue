d383b01f1acdce2cc3c4b001935273683925bdc7
xujia <j538350@163.com>
2025-04-28 23:59:22 +0800

    update

--- frontend/src/views/reportinfo/InterfaceDataView.vue
+++ frontend/src/views/reportinfo/InterfaceDataView.vue
@@ -4,23 +4,45 @@
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted} from 'vue'
-import { useRouter,useRoute, onBeforeRouteUpdate } from 'vue-router'
+<script>
+import { useRoute } from 'vue-router'
 import InterfaceDataViewItem from './components/InterfaceDataViewItem.vue'
 
-const route = useRoute()
-const router = useRouter()
-const interface_id = ref(parseInt(route.query.interface_id))
-const interface_code = ref(route.query.interface_code)
-const interface_name = ref(route.query.interface_name)
-
-router.beforeEach(async (to, from) => {
-    if (to.query.interface_name) {
-        to.meta.title = '接口查询-' + to.query.interface_name
-        console.log('meta title',from.path, to.path)
+export default {
+  name: 'InterfaceDataView',
+  components: {
+    InterfaceDataViewItem
+  },
+  data: () => {
+    const route = useRoute()
+    return {
+      interface_id: parseInt(route.query.interface_id),
+      interface_code: route.query.interface_code,
+      interface_name: route.query.interface_name
     }
-})
+  },
+  beforeRouteEnter(to, from, next) {
+    if (to.query.interface_name) {
+        const title = to.meta.title
+        to.meta.title = title +'-'+ to.query.interface_name
+    }
+    next()
+  }
+
+}
+
+// const route = useRoute()
+// const router = useRouter()
+// const interface_id = ref(parseInt(route.query.interface_id))
+// const interface_code = ref(route.query.interface_code)
+// const interface_name = ref(route.query.interface_name)
+
+// router.beforeEach(async (to, from) => {
+//     if (to.query.interface_name) {
+//         to.meta.title = '接口查询-' + to.query.interface_name
+//         console.log('meta title',from.path, to.path)
+//     }
+// })
 
 // onBeforeRouteUpdate((to, form) => {
 //       console.log('onBeforeRouteUpdate to', to)
