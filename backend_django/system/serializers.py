358e4a91527ccf91a76be476d424390d66fcdd5b
admin <admin@example.com>
2025-05-26 15:29:28 +0800

    update

--- backend_django/system/serializers.py
+++ backend_django/system/serializers.py
@@ -1,31 +1,42 @@
 from rest_framework.utils.serializer_helpers import BindingDict
 from rest_framework import serializers
-from .models import User,Role,Menu, SystemConfig
+from .models import User,Role,Menu, SystemConfig, Dept, SystemDictType,SystemDictData
 from utils.serializer import set_choice_field_internal_value, set_choice_field_representation
 
+class SystemDictTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SystemDictType
+        fields = "__all__"
+
+class SystemDictDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SystemDictData
+        fields = "__all__"
+
 class RoleSerializer(serializers.ModelSerializer):
-    create_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    create_time = serializers.DateTimeField(source='created_at',format='%Y-%m-%d %H:%M:%S', read_only=True)
 
     class Meta:
         model = Role
-        fields = ['id', 'name', 'description', 'create_time']
+        fields = ['id', 'role_name','role_key', 'create_time']
+        read_only_fields = ['id', 'create_time']
+
+class DeptSerializer(serializers.ModelSerializer):
+    create_time = serializers.DateTimeField(source="created_at",format='%Y-%m-%d %H:%M:%S', read_only=True)
+
+    class Meta:
+        model = Dept
+        fields = "__all__"
         read_only_fields = ['id', 'create_time']
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
     create_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True, source='created_at')
-    role_info = RoleSerializer(source='role', read_only=True)
-    role_id = serializers.PrimaryKeyRelatedField(
-        source='role',
-        queryset=Role.objects.all(),
-        required=False,
-        write_only=True
-    )
-    role = serializers.CharField(write_only=True, required=True)
+    dept = DeptSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'role_info', 'role_id', 'create_time','avatar', 'role']
+        fields = ['id', 'username', 'password', 'dept', 'create_time','avatar']
         read_only_fields = ['id', 'create_time']
 
     def create(self, validated_data):
@@ -48,25 +59,17 @@ class UserSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 class MenuSerializer(serializers.ModelSerializer):
-    creator_info = UserSerializer(source='creator', read_only=True)
-    parent_name = serializers.CharField(source='parent.name', read_only=True)
-    created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
-    updated_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
-    meta = serializers.SerializerMethodField()
+    # creator_info = UserSerializer(source='creator', read_only=True)
+    # parent_name = serializers.CharField(source='parent.name', read_only=True)
+    # created_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    # updated_at = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
     
     class Meta:
         model = Menu
-        fields = ['id', 'name','menu_type', 'name_code', 'parent', 'parent_name', 'path', 'component','component_name', 'redirect', 
-                 'icon', 'sort', 'hidden', 'meta', 'creator', 'creator_info', 'created_at', 'updated_at','meta_need_tagview']
+        # fields = "__all__"
+        exclude = ['creator','updator','created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at']
     
-    def get_meta(self, obj):
-        """返回meta对象，包含前端路由需要的meta信息"""
-        return {
-            'title': obj.meta_title or obj.name,
-            'icon': obj.meta_icon or obj.icon,
-            'needTagview': obj.meta_need_tagview or False,
-        }
 
 class SystemConfigSerializer(serializers.ModelSerializer):
     creator_info = UserSerializer(source='creator', read_only=True)
