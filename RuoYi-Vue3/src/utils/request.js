d260cdda00f536cdf3725bf3770db08d5bb7c803
admin <admin@example.com>
2025-06-12 16:16:23 +0800

    处理登录token失效，跳转登陆页面卡顿问题

--- RuoYi-Vue3/src/utils/request.js
+++ RuoYi-Vue3/src/utils/request.js
@@ -5,7 +5,7 @@ import errorCode from '@/utils/errorCode'
 import { tansParams, blobValidate } from '@/utils/ruoyi'
 import cache from '@/plugins/cache'
 import { saveAs } from 'file-saver'
-import useUserStore from '@/store/modules/user'
+import { useRoute } from 'vue-router'
 
 let downloadLoadingInstance
 // 是否显示重新登录
@@ -124,7 +124,9 @@ service.interceptors.response.use(res => {
             isRelogin.show = true
             ElMessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
                 isRelogin.show = false
-                location.href = '/login'
+                // location.href = '/login'
+                route = useRoute()
+                route.push('/login')
             }).catch(() => {
                 isRelogin.show = false
             })
