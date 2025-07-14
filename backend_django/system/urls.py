1da27aaad8ef64183d96c04962a10fe4b2b0509b
admin <admin@example.com>
2025-07-14 09:54:23 +0800

    update

--- backend_django/system/urls.py
+++ backend_django/system/urls.py
@@ -14,14 +14,14 @@ router.register('dict/data', SystemDictDataViewSet, basename='system-dict-data')
 
 urlpatterns = [
     path('system/', include(router.urls)),
+    path('system/dict/data/type/<str:dict_type>', SystemDictDataViewSet.as_view({'get': 'get_data_by_type',})),
+    path('system/dept/exclude/<int:dept_id>', DeptViewSet.as_view({'get': 'exclude',})),
+    path('system/menu/roleMenuTreeselect/<int:roleId>', MenuViewSet.as_view({'get': 'roleMenuTreeselect',}), name='menu-role-treeselect'),
+    path('system/role/deptTree/<int:roleId>', RoleViewSet.as_view({'get': 'deptTree',})),
     path('captchaImage',UserViewSet.as_view({'get': 'captchaImage',})),
     path('login', UserViewSet.as_view({'post': 'login',}), name='user-login'),
     path('getInfo', UserViewSet.as_view({'get': 'getInfo',})),
     path('register', UserViewSet.as_view({'post': 'register',}), name='user-register'),
     path('logout', UserViewSet.as_view({'post': 'logout',})),
-    path('getRouters', MenuViewSet.as_view({'get': 'getRouters',}), name='menu-routers'),
-    path('system/dict/data/type/<str:dict_type>', SystemDictDataViewSet.as_view({'get': 'get_data_by_type',})),
-    path('system/dept/exclude/<int:dept_id>', DeptViewSet.as_view({'get': 'exclude',})),
-    path('system/menu/roleMenuTreeselect/<int:roleId>', MenuViewSet.as_view({'get': 'roleMenuTreeselect',}), name='menu-role-treeselect'),
-    path('system/role/deptTree/<int:roleId>', RoleViewSet.as_view({'get': 'deptTree',}))
+    path('getRouters', MenuViewSet.as_view({'get': 'getRouters',}), name='menu-routers')
 ]
