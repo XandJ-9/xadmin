21f721b42033f2b2cd02e20d86328e34a6a4b2ca
admin <admin@example.com>
2025-08-08 18:04:52 +0800

    update

---
+++ RuoYi-Vue3/src/views/dataassets/report/list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+    <!-- 报表展示列表 -->
+      <splitpanes :horizontal="appStore.device === 'mobile'" class="default-theme">
+        <pane min-size="300" size="50">
+            <!-- 报表分类信息 -->
+             <el-col>
+
+             </el-col>
+        </pane>
+        <pane>
+            <!-- 报表信息 -->
+             <el-col>
+                
+             </el-col>
+        </pane>
+      </splitpanes>
+  </div>
+</template>
+
+<script setup name="ReportList">
+import { ref } from 'vue'
+import useAppStore from '@/store/modules/app'
+import { Splitpanes, Pane } from "splitpanes"
+import "splitpanes/dist/splitpanes.css"
+
+const appStore = useAppStore()
+
+</script>
+
+<style>
+
+</style>
